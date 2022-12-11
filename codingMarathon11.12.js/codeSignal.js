25c9d2e203f4b2cf1ed34863b3a9a992dddad49a
Hasmiktx <hasmiktx@gmail.com>
2022-12-11 20:52:38 +0400

    added killBit

---
+++ codingMarathon11.12.js/codeSignal.js
@@ -0,0 +1,13 @@
+function killBit(n, k) {
+  const binary = n.toString(2);
+  let binaryArr;
+  if (binary.charAt[k] !== 0) {
+    binaryArr = binary.split("");
+    binaryArr[k] = 0;
+    return parseInt(binaryArr.join(""), 2);
+  }
+
+  return n;
+}
+console.log(killBit(37, 3)); //33
+console.log(killBit(37, 4)); //37
