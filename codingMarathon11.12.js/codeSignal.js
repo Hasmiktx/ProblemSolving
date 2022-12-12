c4f8adecdf080e13ed99f30888ea4256943c6b31
Hasmiktx <hasmiktx@gmail.com>
2022-12-12 15:32:43 +0400

    changes

--- codingMarathon11.12.js/codeSignal.js
+++ codingMarathon11.12.js/codeSignal.js
@@ -1,7 +1,8 @@
 function killBit(n, k) {
   const binary = n.toString(2);
+
   let binaryArr;
-  if (binary.charAt[k] !== 0) {
+  if (Number(binary.charAt(k)) === 1) {
     binaryArr = binary.split("");
     binaryArr[k] = 0;
     return parseInt(binaryArr.join(""), 2);
