018783254cc937cc6ac56979ca3b938f0cd5e82d
Hasmiktx <hasmiktx@gmail.com>
2022-12-15 01:40:43 +0400

    deleteDuplicates func

--- Leetcode_problems/deleteDuplicates.js
+++ Leetcode_problems/deleteDuplicates.js
@@ -1,23 +1,33 @@
+/*Given the head of a sorted linked list, delete all duplicates such that each element
+ appears only once.
+  Return the linked list sorted as well. */
+
 // Input: head = [1, 1, 2];
 // Output: [1, 2];
 // Input: head = [1,1,2,3,3]
 // Output: [1,2,3]
 
+/*Time Complexity ???*/
 function ListNode(value) {
   this.value = value;
   this.next = null;
 }
-function makeObj(arr) {
-  let head = new ListNode(arr[0]);
-  let list = head;
-  //   console.log(list.head.value);
-  //   console.log(list.head.next)
-  for (let i = 1; i < arr.length; i++) {
-    list.next = new ListNode(arr[i]);
-    list = list.next;
+
+let deleteDuplicates = function (head) {
+  let list = new ListNode(head[0]);
+  let node = list;
+  for (let i = 1; i < head.length; i++) {
+    node.next = new ListNode(head[i]);
+    node = node.next;
   }
-  return head;
-}
-const head = makeObj([1, 1, 2, 3, 3]);
-console.log(head.next);
-function deleteDuplicates(head) {}
+  let current = list;
+  while (current && current.next) {
+    if (current.value === current.next.value) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+  return list;
+};
+deleteDuplicates([1, 1, 2, 3, 3]);
